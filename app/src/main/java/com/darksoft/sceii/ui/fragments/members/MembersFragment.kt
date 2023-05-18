e41ce9f847ae718e94e52d831c388599595f1538
WhitesSoft <juan.moonteroo@gmail.com>
2023-05-17 22:45:39 -0400

    Abril 17 | Navigation drawer complete | Juan Montero

---
+++ app/src/main/java/com/darksoft/sceii/ui/fragments/members/MembersFragment.kt
@@ -0,0 +1,25 @@
+package com.darksoft.sceii.ui.fragments.members
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.darksoft.sceii.R
+
+class MembersFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_members, container, false)
+    }
+
+}
